0820b999d303bf6fc23318e441a43c479729b631
NatyOrszag <orszagnatanael@gmail.com>
2020-11-27 20:35:47 +0200

    project 1

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,6 +8,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Racing Game" );
+        Car carReference= new Car();
+        carReference.make ="Skoda";
+        carReference.model ="Fabia";
+        carReference.mileage =7.5;
+        carReference.fuellevel = 60;
+        carReference.color ="black";
+        carReference.maxSpeed = 300;
+
+        System.out.println(carReference.make);
+        System.out.println(carReference.model);
+        System.out.println(carReference.mileage);
+        System.out.println(carReference.fuellevel);
+        System.out.println(carReference.color);
+        System.out.println(carReference.maxSpeed);
+
+
+
     }
 }
