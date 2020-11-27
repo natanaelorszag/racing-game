890dca46e478f84875330d9c91b806d32c3fb087
NatyOrszag <orszagnatanael@gmail.com>
2020-11-27 18:27:20 +0200

    project 1

---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,17 @@
+package org.fasttrackit;
+
+public class Car {
+    String make;
+    String model;
+    String color;
+
+    double mileage;
+    double fuellevel;
+    double maxSpeed;
+    double totalTravelDistance;
+    boolean damaged;
+    int doorCount;
+
+
+
+}
